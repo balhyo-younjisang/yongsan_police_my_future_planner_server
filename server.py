57a3526eb5d71b29ddb6651e262207e8620ae949
eunsssang12 <codeunsang30@gmail.com>
2025-06-08 21:48:20 +0900

    서버코드

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))) 
+
+import models
+from database import engine 
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
